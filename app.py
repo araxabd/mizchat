53ce51b9e970ef0b41890ba644c355d9c13e8cb2
araxabd <arianabdollahian@gmail.com>
2025-07-25 00:55:02 +0330

    Very basic chatroom

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from dotenv import dotenv_values
 
+from db import init, save_msg, get_all_msg
+
 app = Flask(__name__)
 
 # load the configs from .env file as a dictionary
 conf = dotenv_values('.env')
 app.config["SECRET_KEY"] = conf["SECRET_KEY"] # Set < Secret Key > based on the environment
 
+# Start WebSocket
 ws = SocketIO(app)
+
+# Initialize database
+init()
 
 
 @app.route('/')
@@ -16,10 +22,11 @@ def home():
     return render_template("index.html")
 
 
-@ws.on('test')
-def test(data):
+@ws.on('msg')
+def message(data):
     print(f'{data} is received')
-    emit("msg", f"{data} is received")
+    save_msg(data)
+    emit("messages", get_all_msg())
 
 if __name__ == '__main__':
     ws.run(app, debug=True)
