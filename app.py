1fd634ebc50c38812dd17135d28e267bffe21526
araxabd <arianabdollahian@gmail.com>
2025-07-24 21:31:32 +0330

    Set flask configs

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from dotenv import dotenv_values
 
 app = Flask(__name__)
+
+# load the configs from .env file as a dictionary
+conf = dotenv_values('.env')
+
+app.config["SECRET_KEY"] = conf["SECRET_KEY"] # Set < Secret Key > based on the environment
 
 @app.route('/')
 def home():
