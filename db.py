0c06ca8ba3d8d78013e50e40e72459696313a951
araxabd <arianabdollahian@gmail.com>
2025-07-25 22:24:15 +0330

    Create prefer tables

--- db.py
+++ db.py
@@ -7,7 +7,21 @@ db_url = conf["DATABASE_URL"]
 def init():
     conn = sqlite3.connect(db_url)
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS messages (context TEXT);")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS users
+    (id INTEGER PRIMARY KEY AUTOINCREMENT
+    username TEXT UNIQUE NOT NULL,
+    passhash TEXT NOT NULL);
+    """)
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS messages 
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    user INTEGER NOT NULL,
+    room TEXT NOT NULL,
+    time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user) REFRENCES users (id));""")
+    
     conn.commit()
     conn.close()
 
